bad4b9d5f4f56c8c92f498aed25b1a3ebbbbfa3a
koenry <sarunas.suchovas@gmail.com>
2022-02-28 14:49:16 +0200

    Added XMAS app

---
+++ 10. Xmas App/script.js
@@ -0,0 +1,14 @@
+$('.btn1').click(function(){
+    if($(this).hasClass('active')){
+        $(this).removeClass('active')
+    } else {
+        $(this).addClass('active')
+    }
+});
+
+function myFunction() {
+    document.querySelector(".example").style.backgroundColor = "red";
+  }
+
+
+ 
